52bc4beda7beae57c04f04eac56f8a4038657635
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-21 15:09:49 +0200

    add screenshots.js

---
+++ screenshots.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+CHART_BASE_URL = 'https://covid-19.sledilnik.org/embed.html#/chart/';
+CARD_BASE_URL = 'https://covid-19.sledilnik.org/';
+
+CHART = {
+  IcuPatients: {
+    name: 'IcuPatients',
+  },
+  Patients: {
+    name: 'Patients',
+  },
+};
+
+CARD = {
+  testsToday: { name: 'testsToday' },
+  testsTodayHAT: { name: 'testsTodayHAT' },
+  casesActive: { name: 'casesActive' },
+  casesAvg7Days: { name: 'casesAvg7Days' },
+  hospitalizedCurrent: { name: 'hospitalizedCurrent' },
+  icuCurrent: { name: 'icuCurrent' },
+  deceasedToDate: { name: 'deceasedToDate' },
+  vaccinationSummary: { name: 'vaccinationSummary' },
+};
+
+OPTIONS = {
+  CARD: {
+    viewport: { width: 325, height: 800 },
+    getSelector: type => `.cardtype-${type} > .hp-card`,
+    getUrl: () => CARD_BASE_URL,
+  },
+  CHART: {
+    viewport: { width: 1200, height: 800 },
+    getSelector: () => '.visualization.container.embeded > section',
+    getUrl: type => `${CHART_BASE_URL}${type}`,
+    selectorToRemove: '.metrics-selectors',
+  },
+};
+
+module.exports = { OPTIONS, CARD, CHART };
