5e40ba8c7220609f9a906e90f90d17537804283e
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-21 15:23:40 +0200

    index: send image

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event, context, callback) => {
+  if (!event.queryStringParameters) {
+    return callback(undefined, 'No target');
+  }
+
+  const { type: _type, screen: chosenScreen } = event.queryStringParameters;
+  const type = _type.toUpperCase();
+
+  if (!type || !chosenScreen) {
+    return callback(
+      undefined,
+      `Missing query parameters: type = ${type}, screen = ${chosenScreen}`
+    );
+  }
+
+  if (!Object.keys(screenshots).includes(type)) {
+    throw new Error(`Invalid type: ${type}`);
+  }
+
+  const options = screenshots.OPTIONS[type];
+
+  const { viewport, getSelector, getUrl, selectorToRemove } = options;
+  const possibleScreenshots = screenshots[type];
+
+  if (
+    !possibleScreenshots ||
+    !Object.keys(possibleScreenshots).includes(chosenScreenshot)
+  ) {
+    throw new Error(`Invalid chosen screenshot: ${chosenScreenshot}`);
+  }
+
+  const screenshot = possibleScreenshots[chosenScreenshot];
+
+  const url = getUrl(screenshot.name);
+  const selector = getSelector(screenshot.name);
+
   let result = null;
   let browser = null;
 
@@ -14,15 +49,47 @@ exports.handler = async (event, context, callback) => {
     });
     console.log('Browser launched');
 
-    let page = await browser.newPage();
-    console.log('Made page.');
+    const page = await browser.newPage();
+    console.log('Made Page');
 
-    await page.goto(event.url || 'https://example.com', {
-      waitUntil: 'networkidle0',
-    });
-    console.log('Navigated to page');
+    await page.setViewport(viewport);
+    console.log('Set viewport');
 
-    result = await page.title();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    console.log('Went to ', url);
+
+    if (selectorToRemove) {
+      await page.evaluate(sel => {
+        const elements = document.querySelectorAll(sel);
+        for (let i = 0; i < elements.length; i++) {
+          elements[i].parentNode.removeChild(elements[i]);
+        }
+      }, selectorToRemove);
+      console.log('Selector removed');
+    }
+
+    const element = await page.$(selector);
+    if (!element) {
+      return { message: "Wrong selector or it's not visible" };
+    }
+
+    image = await element.screenshot({ type: 'png' });
+    console.log('Made screenshot');
+
+    const filename = `${new Date()
+      .toISOString()
+      .slice(0, 10)}---${chosenScreenshot}.png`;
+    console.log('Filename is ', filename);
+
+    result = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/png',
+        'Content-Disposition': `attachment; filename="${filename}"`,
+      },
+      body: image.toString('base64'),
+      isBase64Encoded: true,
+    };
   } catch (error) {
     return callback(error);
   } finally {
