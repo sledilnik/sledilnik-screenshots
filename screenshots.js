5531c75c43c20a7cb2060bee1722c778f3b20bf0
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-22 16:35:29 +0200

    screenshots add  all possible charts

--- screenshots.js
+++ screenshots.js
@@ -10,6 +10,54 @@ CHART = {
   Patients: {
     name: 'Patients',
   },
+  MetricsComparison: {
+    name: 'MetricsComparison',
+  },
+  EuropeMap: {
+    name: 'EuropeMap',
+  },
+  WorldMap: {
+    name: 'WorldMap',
+  },
+  CarePatients: {
+    name: 'CarePatients',
+  },
+  Ratios: {
+    name: 'Ratios',
+  },
+  Tests: {
+    name: 'Tests',
+  },
+  HCenters: {
+    name: 'HCenters',
+  },
+  Cases: {
+    name: 'Cases',
+  },
+  Spread: {
+    name: 'Spread',
+  },
+  Infections: {
+    name: 'Infections',
+  },
+  Regions: {
+    name: 'Regions',
+  },
+  RegionMap: {
+    name: 'RegionMap',
+  },
+  Map: {
+    name: 'Map',
+  },
+  Municipalities: {
+    name: 'Municipalities',
+  },
+  SchoolStatus: {
+    name: 'SchoolStatus',
+  },
+  AgeGroups: {
+    name: 'AgeGroups',
+  },
 };
 
 CARD = {
