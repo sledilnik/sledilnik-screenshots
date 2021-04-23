ec73f9889e490cdf8b9bc8a3f22818f44b26e616
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-23 16:11:36 +0200

    introduce navigatToCustomChart

---
+++ navigateToCustomChart.js
@@ -0,0 +1,30 @@
+module.exports = async ({
+  page,
+  element,
+  screenshot,
+  customChartName,
+  chosenScreenshot,
+}) => {
+  const stepsToReproduce = screenshot.customChart[customChartName];
+  if (!stepsToReproduce) {
+    throw new Error(
+      `No custom ${customChartName} chart for chart: ${chosenScreenshot}`
+    );
+  }
+  const buttons = {
+    interval: await element.$$(
+      '.chart-data-interval-selector > .chart-display-property-selector__item'
+    ),
+    display: await element.$$(
+      '.chart-display-property-selector > .chart-display-property-selector__item'
+    ),
+  };
+
+  for (item of stepsToReproduce) {
+    const [what, which, func] = item;
+    const button = buttons[what][which];
+    const text = await page.evaluate(el => el.textContent, button);
+    await func(button);
+    console.log(`Button "${text}" clicked`);
+  }
+};
