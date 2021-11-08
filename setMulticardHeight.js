da64105e01887b467c31bbec64b3efeb8439c6c4
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-08 20:50:10 +0100

    FEAT: set footer height
    after we removed social icons and email in footer (see: 6f4e573),
    we need to set some footer children styling and set footer height

--- setMulticardHeight.js
+++ setMulticardHeight.js
@@ -4,32 +4,49 @@ exports.setMulticardHeight = async (page, viewport) => {
     el.style.marginTop = '0';
   });
 
-  const nav = await page.$eval('.navbar-container', el => ({
-    height: el.clientHeight,
-    margin: el.style.margin,
-  }));
-  const { height: navHeight, margin: navMargin } = nav;
+  const nav = await page.$('.navbar-container');
+  const navBB = await nav.boundingBox();
+  const { height: navHeight } = navBB;
 
-  const ts = await page.$eval('.time-stamp', el => {
+  await page.$eval('.time-stamp', el => {
     el.style.margin = '0';
-    return { height: el.clientHeight, margin: el.style.margin };
+    return;
   });
-  const { height: tsHeight, margin: tsMargin } = ts;
+  const ts = await page.$('.time-stamp');
+  const tsBB = await ts.boundingBox();
+  const { height: tsHeight } = tsBB;
 
-  const cw = await page.$eval('.cards-wrapper', el => {
-    el.style.margin = '12px 0';
-    return { height: el.clientHeight, margin: el.style.margin };
+  const cwMargin = await page.$eval('.cards-wrapper', el => {
+    el.style.margin = '12px 0px';
+    return el.style.margin;
   });
-  const { height: cwHeight, margin: cwMargin } = cw;
+  const cw = await page.$('.cards-wrapper');
+  const cwBB = await cw.boundingBox();
+  const { height: cwHeight } = cwBB;
 
-  const footer = await page.$eval('footer', el => ({
-    height: el.clientHeight,
-    margin: el.style.margin,
-  }));
-  const { height: footerHeight, margin: footerMargin } = footer;
+  await page.$eval('div.content-wrapper', el => {
+    el.style.lineHeight = 1;
+    return;
+  });
 
-  const marginTop = +cwMargin.split(' ')?.[0]?.replace('px', '') * 2;
-  const newHeight = navHeight + tsHeight + cwHeight + footerHeight + marginTop;
+  await page.$eval('div.copy', el => {
+    el.style.margin = '0px';
+    return;
+  });
+
+  await page.$eval('footer', el => {
+    el.style.minHeight = '46px';
+    el.style.height = '46px';
+    return;
+  });
+  const footer = await page.$('footer');
+  const footerBB = await footer.boundingBox();
+  const { height: footerHeight } = footerBB;
+
+  const marginHeight = +cwMargin.split(' ')?.[0]?.replace('px', '') * 2;
+  const newHeight = Math.round(
+    navHeight + tsHeight + cwHeight + footerHeight + marginHeight
+  );
 
   await page.setViewport({ width: viewport.width, height: newHeight });
   console.log(`Adjust viewport height: ${newHeight}`);
